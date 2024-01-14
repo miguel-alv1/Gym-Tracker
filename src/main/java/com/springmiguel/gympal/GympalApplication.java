44bc538265f7c91bd59e4b6abefcbc6d1a2fd03e
miguel-alv1 <miguel.alvarez9724@gmail.com>
2024-01-13 18:17:40 -0600

    initial commit - skeleton spring code

---
+++ src/main/java/com/springmiguel/gympal/GympalApplication.java
@@ -0,0 +1,13 @@
+package com.springmiguel.gympal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GympalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GympalApplication.class, args);
+	}
+
+}
